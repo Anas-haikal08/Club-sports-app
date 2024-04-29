942af4277d6352a060d2d8b890c6ef274cf7c28b
Anashaikal-8 <anashaikal184@gmail.com>
2024-04-29 15:59:59 +0300

    copy

--- src/pages/auth/AuthWrapper.tsx
+++ src/pages/auth/AuthWrapper.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Col, Row} from 'antd';
+import { Col, Row } from 'antd';
 
 import './AuthWrapper.style.less';
 
-const AuthWrapper = ({children}: any) => {
+const AuthWrapper = ({ children }: any) => {
   return (
     <Row className='sign-in'>
       <Col xl={12} md={12} xs={24}>
